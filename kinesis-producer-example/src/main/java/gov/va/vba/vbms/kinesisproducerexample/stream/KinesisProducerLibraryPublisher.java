86997bfdc1d1ff8de4e43712a2dc6c491ff695b1
markmixson <mixson_mark@bah.com>
2020-06-16 10:29:43 -0400

    clean up spring configuration usage

--- kinesis-producer-example/src/main/java/gov/va/vba/vbms/kinesisproducerexample/stream/KinesisProducerLibraryPublisher.java
+++ kinesis-producer-example/src/main/java/gov/va/vba/vbms/kinesisproducerexample/stream/KinesisProducerLibraryPublisher.java
@@ -9,34 +9,40 @@ import com.google.common.util.concurrent.ListenableFuture;
 import gov.va.vba.vbms.kinesisexamplecommons.stream.Event;
 import gov.va.vba.vbms.kinesisexamplecommons.stream.PublishInfo;
 import gov.va.vba.vbms.kinesisexamplecommons.stream.Publisher;
-import gov.va.vba.vbms.kinesisproducerexample.KinesisProducerExampleConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class KinesisProducerLibraryPublisher implements Publisher<Event, List<ListenableFuture<UserRecordResult>>> {
 
-    private final KinesisProducerExampleConfiguration config = new KinesisProducerExampleConfiguration();
-    private final KinesisProducer kinesisProducer = new KinesisProducer(getKinesisProducerConfiguration());
+    private final KinesisProducerConfiguration kinesisProducerConfiguration;
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    KinesisProducerLibraryPublisher(KinesisProducerConfiguration kinesisProducerConfiguration) {
+        this.kinesisProducerConfiguration = kinesisProducerConfiguration;
+    }
 
     @Override
     public List<ListenableFuture<UserRecordResult>> publish(List<Event> data, PublishInfo info) {
-        List<ListenableFuture<UserRecordResult>> results = new ArrayList<>();
-        for (Event event : data) {
-            ByteBuffer byteBuffer = ByteBuffer.wrap(getSerializedEvent(event));
-            ListenableFuture<UserRecordResult> future =
-                    kinesisProducer.addUserRecord(info.getStreamName(), info.getPartitionKey(), byteBuffer);
-            results.add(future);
-        }
-        kinesisProducer.flushSync();
+        List<ListenableFuture<UserRecordResult>> results = getResults(data, info);
         log.info("finished writing " + results.size() + " events to stream");
+        return results;
+    }
+
+    private List<ListenableFuture<UserRecordResult>> getResults(List<Event> data, PublishInfo info) {
+        KinesisProducer kinesisProducer = new KinesisProducer(kinesisProducerConfiguration);
+        List<ListenableFuture<UserRecordResult>> results = data.stream()
+                .map(event -> {
+                    ByteBuffer byteBuffer = ByteBuffer.wrap(getSerializedEvent(event));
+                    return kinesisProducer.addUserRecord(info.getStreamName(), info.getPartitionKey(), byteBuffer);
+                })
+                .collect(Collectors.toList());
+        kinesisProducer.flushSync();
         return results;
     }
 
@@ -45,14 +51,6 @@ public class KinesisProducerLibraryPublisher implements Publisher<Event, List<Li
             return objectMapper.writeValueAsBytes(event);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("can't build serialized event!", e);
-        }
-    }
-
-    private KinesisProducerConfiguration getKinesisProducerConfiguration() {
-        try {
-            return config.kinesisProducerConfiguration();
-        } catch (URISyntaxException e) {
-            throw new RuntimeException("cannot get configuration!", e);
         }
     }
 }
